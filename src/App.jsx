fee06c309a875019bd40652322c782d21bb5e16a
akrameza1995 <61572601+akrameza1995@users.noreply.github.com>
2025-02-01 11:47:53 -0800

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function HomeStagingApp() {
+  return (
+    <div>
+      <h1>Welcome to Timeless Decor</h1>
+      <p>Enter your home address or Airbnb link to get personalized decor recommendations.</p>
+    </div>
+  );
+}
+
+export default HomeStagingApp;
